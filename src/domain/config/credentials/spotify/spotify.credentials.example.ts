6f6193b9006b6324d7e0d14d2168e858ae394569
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-01 23:33:06 -0300

    chore: created spotify config

---
+++ src/domain/config/credentials/spotify/spotify.credentials.example.ts
@@ -0,0 +1,6 @@
+export default {
+  /** @argument ClientID - Você pode registrar seu app para conseguir suas credenciais em https://developer.spotify.com/dashboard/applications */
+  ClientID: "",
+  /** @argument ClientSecret - Você pode registrar seu app para conseguir suas credenciais em https://developer.spotify.com/dashboard/applications */
+  ClientSecret: "",
+};
