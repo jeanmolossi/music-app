1e0c1ff9c87525e344cfd8f1822cec2750d89db9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-01 23:33:44 -0300

    chore: changed imports and behavior on scroll

--- src/presentation/pages/home/components/albums/albums-list.tsx
+++ src/presentation/pages/home/components/albums/albums-list.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Alert, Image, View } from "react-native";
+import {
+  FlatList,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
-import { Alert, FlatList, Image, Pressable, View } from "react-native";
-import { Theme } from "../../../../styles";
-import styles from "../../styles";
-import { Text } from "../../../../components";
+import { Theme } from "@/presentation/styles";
+import { Text } from "@/presentation/components";
+import styles from "@/presentation/pages/home/styles";
 import { useCallback } from "react";
 
 interface AlbumsListProps {
@@ -23,15 +27,24 @@ export const AlbumsList = ({ data }: AlbumsListProps) => {
       contentContainerStyle={styles.albums_flat_list_container}
       keyExtractor={(_, index) => index.toString()}
       renderItem={({ item }) => (
-        <View style={styles.algums_item}>
+        <View
+          style={styles.algums_item}
+          onStartShouldSetResponder={() => true}
+          onStartShouldSetResponderCapture={() => true}
+          onMoveShouldSetResponderCapture={() => true}
+          onMoveShouldSetResponder={() => true}
+        >
           <Image style={styles.album_image} source={{ uri: item.cover }} />
           <View style={styles.album_middle_content}>
             <Text>{item.title}</Text>
             <Text variant="dark">{item.year}</Text>
           </View>
-          <Pressable onPress={moreOption} style={styles.album_more_button}>
+          <TouchableWithoutFeedback
+            onPress={moreOption}
+            style={styles.album_more_button}
+          >
             <Feather name="more-horizontal" size={20} color={Theme.mainColor} />
-          </Pressable>
+          </TouchableWithoutFeedback>
         </View>
       )}
     />
