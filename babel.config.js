ed12409eb469b1440aa08cd80e453aa0955fcb14
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-01 23:32:35 -0300

    chore: added module resolver paths config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["src"],
+          extensions: [".js", ".jsx", ".es", ".es6", ".mjs", ".ts", ".tsx"],
+          alias: {
+            "@": "./src",
+          },
+        },
+      ],
+    ],
   };
 };
