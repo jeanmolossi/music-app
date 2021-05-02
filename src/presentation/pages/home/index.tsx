611ee707e9ab22f686c9518acba1e1efcb915b9a
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-01 23:39:01 -0300

    chore: uses metadatas

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -4,12 +4,18 @@ import { PanGestureHandler } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
 import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { Player, Text, usePlayer } from "../../components";
-import db from "../../../data/mock/fake_db.json";
-import { Theme } from "../../styles";
-import styles from "./styles";
+import {
+  Player,
+  Text,
+  usePlayer,
+  usePlayerContext,
+  PlayerState,
+} from "@/presentation/components";
+import db from "@/data/mock/fake_db.json";
+import { Theme } from "@/presentation/styles";
 import { PopularList, AlbumsList, SocialsInfo } from "./components";
 import { useHomeHelpers } from "./helpers";
+import styles from "./styles";
 
 interface HomeProps {}
 
@@ -22,11 +28,15 @@ export const Home = ({}: HomeProps) => {
   } = useHomeHelpers();
 
   const { onClose, visibility, onOpen } = usePlayer();
+  const { playbackState, currentTrackMetadata } = usePlayerContext();
 
   return (
     <View style={styles.container}>
       <Player {...{ onClose, visibility, onOpen }} />
-      <ImageBackground source={{ uri: db.cover }} style={styles.imageCover}>
+      <ImageBackground
+        source={{ uri: currentTrackMetadata.cover }}
+        style={styles.imageCover}
+      >
         <Animated.View style={[textBoxAnimation, { width: "100%" }]}>
           <LinearGradient
             colors={["#000000", "#00000000"]}
@@ -36,7 +46,7 @@ export const Home = ({}: HomeProps) => {
             <Animated.View style={[styles.track_info]}>
               <Text style={styles.track_info_badge}>ARTISTA</Text>
               <Animated.Text style={[styles.track_info_music, textAnimation]}>
-                Eminem / Vicente
+                {currentTrackMetadata.artist}
               </Animated.Text>
             </Animated.View>
           </LinearGradient>
@@ -47,10 +57,18 @@ export const Home = ({}: HomeProps) => {
         <Animated.View style={[styles.more_content, animatedStyle]}>
           <Pressable style={styles.play_button} onPress={onOpen}>
             <LinearGradient
-              style={styles.background_play_button}
+              style={[
+                styles.background_play_button,
+                playbackState === PlayerState.PLAYING
+                  ? { paddingLeft: 0 }
+                  : null,
+              ]}
               colors={[Theme.mainColor, Theme.secondColor]}
             >
-              <FontAwesome5 name="play" size={24} />
+              <FontAwesome5
+                name={playbackState === PlayerState.PLAYING ? "pause" : "play"}
+                size={24}
+              />
             </LinearGradient>
           </Pressable>
 
