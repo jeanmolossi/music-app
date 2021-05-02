b90ec99ff064fbb36b548cd417b839c70c754cab
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-01 23:34:14 -0300

    chore: changed exports behavior of player

---
+++ src/presentation/components/Player/index.ts
@@ -0,0 +1,7 @@
+export * from "./Player";
+export * from "./context/helpers";
+export * from "./context/usePlayerControls";
+export * from "./controls";
+export * from "./helpers";
+export * from "./usePlayer";
+export * from "./styles";
