df0e467737504d18b7aede60d6829c5bc1233763
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-03 00:59:31 -0300

    chore: fixed path on makeApi factory

--- src/main/factories/usecases/remote-load-current-user-info-factory.ts
+++ src/main/factories/usecases/remote-load-current-user-info-factory.ts
@@ -3,7 +3,4 @@ import { makeApiUrl } from "../http/api-url-factory";
 import { makeSpotifyAuthorizedHttpClient } from "../http/spotify-authorized-http-client";
 
 export const makeRemoteLoadCurrentUserInfo = () =>
-  new RemoteLoadUserInfo(
-    makeApiUrl("/v1/me"),
-    makeSpotifyAuthorizedHttpClient()
-  );
+  new RemoteLoadUserInfo(makeApiUrl("/me"), makeSpotifyAuthorizedHttpClient());
