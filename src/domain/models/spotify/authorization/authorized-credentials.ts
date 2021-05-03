6016fa88fd76e667a6c48c9eeefabed17e95eb70
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-02 22:58:56 -0300

    chore: created authorized spotify credentials model

---
+++ src/domain/models/spotify/authorization/authorized-credentials.ts
@@ -0,0 +1,7 @@
+export interface AuthorizedSpotifyCredentials {
+  access_token: string;
+  token_type: "Bearer";
+  scope: string;
+  expires_in: number;
+  refresh_token: string;
+}
---
+++ src/domain/models/spotify/index.ts
@@ -0,0 +1,1 @@
+export * from "./authorization/authorized-credentials";
