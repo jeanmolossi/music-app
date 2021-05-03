4599813bc80d33643cf169a6ef23a20a4d1052ec
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-03 01:16:44 -0300

    chore: changed render infos

--- src/presentation/pages/home/components/socials/socials-info.tsx
+++ src/presentation/pages/home/components/socials/socials-info.tsx
@@ -3,22 +3,30 @@ import { View } from "react-native";
 import { Text } from "../../../../components";
 import styles from "../../styles";
 
-interface SocialsInfoProps {}
+interface SocialsInfoProps {
+  name?: string;
+  followers?: number;
+  country?: string;
+}
 
-export const SocialsInfo = ({}: SocialsInfoProps) => {
+export const SocialsInfo = ({
+  name = "John Doe",
+  followers = 0,
+  country = "BR",
+}: SocialsInfoProps) => {
   return (
     <View style={styles.socials_info}>
       <View style={styles.social_data}>
-        <Text style={styles.social_label}>Songs</Text>
-        <Text style={styles.social_text}>119</Text>
+        <Text style={styles.social_label}>Nome</Text>
+        <Text style={styles.social_text}>{name}</Text>
       </View>
       <View style={styles.social_data}>
         <Text style={styles.social_label}>Followers</Text>
-        <Text style={styles.social_text}>67.9k</Text>
+        <Text style={styles.social_text}>{followers}</Text>
       </View>
       <View style={styles.social_data}>
-        <Text style={styles.social_label}>Following</Text>
-        <Text style={styles.social_text}>3.159</Text>
+        <Text style={styles.social_label}>País</Text>
+        <Text style={styles.social_text}>{country}</Text>
       </View>
     </View>
   );
