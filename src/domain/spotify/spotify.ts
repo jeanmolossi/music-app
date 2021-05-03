f9be2586205d06fcc47f5bec22908528f1c62344
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-03 01:23:34 -0300

    chore: added new spotify model

--- src/domain/spotify/spotify.ts
+++ src/domain/spotify/spotify.ts
@@ -1,5 +1,8 @@
 import { AuthorizedSpotifyCredentials } from "./authorization/authorized-credentials";
+import { CurrentlyPlayingTrackUser } from "./currently-playing-track/currently-playing-track";
 
 export namespace Spotify {
   export type AuthCredentials = AuthorizedSpotifyCredentials;
+
+  export type CurrentlyPlayingTrack = CurrentlyPlayingTrackUser;
 }
