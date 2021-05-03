8088ff60234fe2174bb579b76ad7522968dee03e
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-03 00:58:47 -0300

    refactor: changed behavior to check tokens and set auth

--- src/main/decorators/spotify-authorize-http-client-decorator/spotify-authorize-http-client-decorator.ts
+++ src/main/decorators/spotify-authorize-http-client-decorator/spotify-authorize-http-client-decorator.ts
@@ -1,9 +1,8 @@
-import { encode } from "react-native-base64";
 import spotifyCredentials from "@/domain/config/credentials/spotify/spotify.credentials";
+import base64 from "react-native-base64";
 import { HttpClient } from "@/data/protocols/http/http-client";
 import { GetStorage, SetStorage } from "@/data/protocols/cache";
-import { makeApiUrl } from "@/main/factories/http/api-url-factory";
-import { Spotify } from "@/domain/models/spotify";
+import { Spotify } from "@/domain/spotify";
 
 export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
   implements HttpClient<T> {
@@ -12,11 +11,11 @@ export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
     private readonly httpClient: HttpClient<T>
   ) {}
 
-  async request<BodyType extends URLSearchParams>(
+  async request<BodyType = any>(
     params: HttpClient.Params<BodyType>
   ): Promise<T> {
     let Authorization: string;
-    const token = await this.getRefreshToken();
+    const token = await this.getToken();
 
     if (!token) {
       const {
@@ -27,13 +26,14 @@ export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
       Authorization = `${token_type} ${access_token}`;
 
       await this.storageAdapter.set("refresh_token", refresh_token);
+      await this.storageAdapter.set("access_token", access_token);
     } else {
       Authorization = `Bearer ${token}`;
     }
 
     try {
       Object.assign(params, {
-        headers: Object.assign(params.headers, {
+        headers: Object.assign(params.headers || {}, {
           "Content-Type": "application/x-www-form-urlencoded",
           Authorization,
         }),
@@ -47,13 +47,13 @@ export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
     }
   }
 
-  private async getRefreshToken(): Promise<string | null> {
+  private async getToken(): Promise<string | null> {
     try {
-      const isset_refresh_token = await this.storageAdapter.get<string>(
-        "refresh_token"
+      const isset_access_token = await this.storageAdapter.get<string>(
+        "access_token"
       );
 
-      return isset_refresh_token;
+      return isset_access_token;
     } catch (error) {
       return null;
     }
@@ -61,24 +61,44 @@ export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
 
   private async getValidToken(): Promise<Spotify.AuthCredentials> {
     const code = await this.storageAdapter.get<string>("authorization_code");
+    const refresh_token = await this.storageAdapter.get<string>(
+      "refresh_token"
+    );
 
     if (!code) throw new Error("Code is empty");
 
+    let current_token = code;
+    let grant_type = "authorization_code";
+    let Authorization = `Basic ${this.encode_credentials()}`;
+
     const data = {
-      grant_type: "authorization_code",
-      code,
+      grant_type,
+      code: current_token,
       redirect_uri: encodeURIComponent("http://response.ok"),
     };
 
-    const auth_data = (await this.httpClient.request({
-      method: "POST",
-      url: makeApiUrl("/api/token"),
-      body: new URLSearchParams(data),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Basic ${this.encode_credentials()}`,
-      },
-    })) as unknown;
+    let request_params;
+
+    if (refresh_token) {
+      grant_type = "refresh_token";
+      Authorization = `Bearer ${refresh_token}`;
+      request_params = this.default_request([
+        { grant_type },
+        { refresh_token: Authorization },
+      ]);
+    } else {
+      request_params = this.default_request([{ Authorization }]);
+
+      Object.assign(request_params, {
+        body: new URLSearchParams(data).toString(),
+      });
+    }
+
+    console.log({ request_params });
+
+    const auth_data = (await this.httpClient.request(request_params)) as any;
+
+    console.log(auth_data.request);
 
     return auth_data as Spotify.AuthCredentials;
   }
@@ -86,6 +106,19 @@ export class SpotifyAuthorizeHttpClientDecorator<T = unknown>
   private encode_credentials(): string {
     const { ClientID, ClientSecret } = spotifyCredentials;
 
-    return encode(`${ClientID}:${ClientSecret}`);
+    return base64.encode(`${ClientID}:${ClientSecret}`);
+  }
+
+  private default_request(headers: Array<Record<string, string>>) {
+    return {
+      method: "POST" as const,
+      url: "https://accounts.spotify.com/api/token",
+      headers: Object.assign(
+        {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        ...(headers || {})
+      ),
+    };
   }
 }
