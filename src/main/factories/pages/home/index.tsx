da6b36a4f9d3183d332cedee201278ec021f86ce
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-03 01:15:34 -0300

    chore: resolved home factory page

--- src/main/factories/pages/home/index.tsx
+++ src/main/factories/pages/home/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Home } from "@/presentation/pages";
 import { makeRemoteLoadCurrentUserInfo } from "@/main/factories/usecases";
+import { setSpotifyAuthorizationCodeAdapter } from "@/main/adapters";
 
 interface HomePageFactoryProps {}
 
 export const HomePageFactory = ({}: HomePageFactoryProps) => {
   const remoteLoadCurrentUserInfo = makeRemoteLoadCurrentUserInfo();
+  const setSpotifyAuthorizationCode = setSpotifyAuthorizationCodeAdapter;
 
-  return <Home {...{ remoteLoadCurrentUserInfo }} />;
+  return (
+    <Home
+      {...{
+        remoteLoadCurrentUserInfo,
+        setSpotifyAuthorizationCode,
+      }}
+    />
+  );
 };
