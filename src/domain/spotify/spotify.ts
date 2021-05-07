416be95f523821464ce04727156adc04221926c2
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-06 23:10:39 -0300

    chore: added recently played model

--- src/domain/spotify/spotify.ts
+++ src/domain/spotify/spotify.ts
@@ -1,6 +1,7 @@
 import { AuthorizedSpotifyCredentials } from "./authorization/authorized-credentials";
 import { CurrentlyPlayingTrackUser } from "./currently-playing-track/currently-playing-track";
 import { MyPlaylists as SpotifyMyPlaylists } from "./my-playlists/my-playlists";
+import { RecentlyPlayed as SpotifyRecentlyPlayed } from "./recently-played/recently-played";
 
 export namespace Spotify {
   export type AuthCredentials = AuthorizedSpotifyCredentials;
@@ -8,4 +9,6 @@ export namespace Spotify {
   export type CurrentlyPlayingTrack = CurrentlyPlayingTrackUser;
 
   export type MyPlaylist = SpotifyMyPlaylists;
+
+  export type RecentlyPlayed = SpotifyRecentlyPlayed;
 }
