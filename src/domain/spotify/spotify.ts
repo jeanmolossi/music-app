a65173bc179cb1ae2acefa8bf7361cb908122ab9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-06 22:08:16 -0300

    feat: created myplaylists model

--- src/domain/spotify/spotify.ts
+++ src/domain/spotify/spotify.ts
@@ -1,8 +1,11 @@
 import { AuthorizedSpotifyCredentials } from "./authorization/authorized-credentials";
 import { CurrentlyPlayingTrackUser } from "./currently-playing-track/currently-playing-track";
+import { MyPlaylists as SpotifyMyPlaylists } from "./my-playlists/my-playlists";
 
 export namespace Spotify {
   export type AuthCredentials = AuthorizedSpotifyCredentials;
 
   export type CurrentlyPlayingTrack = CurrentlyPlayingTrackUser;
+
+  export type MyPlaylist = SpotifyMyPlaylists;
 }
