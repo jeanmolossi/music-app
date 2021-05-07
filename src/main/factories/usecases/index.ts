5ac74230062bca4fea7d54ee6639b932c4b8ca66
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-06 23:25:53 -0300

    chore: added recently played usecase factory

--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from "./remote-get-currently-playing-track";
 export * from "./remote-get-my-playlists";
+export * from "./remote-get-recently-played";
 export * from "./remote-load-current-user-info-factory";
