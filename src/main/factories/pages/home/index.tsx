c1eb6303a628e11649d955b5aed7159ed857bcab
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-06 23:28:18 -0300

    chore: added remote get recently played in home factory

--- src/main/factories/pages/home/index.tsx
+++ src/main/factories/pages/home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Home } from "@/presentation/pages";
 import {
   makeRemoteGetMyPlaylists,
+  makeRemoteGetRecentlyPlayed,
   makeRemoteLoadCurrentUserInfo,
 } from "@/main/factories/usecases";
 import { setSpotifyAuthorizationCodeAdapter } from "@/main/adapters";
@@ -12,6 +13,7 @@ export const HomePageFactory = ({}: HomePageFactoryProps) => {
   const remoteLoadCurrentUserInfo = makeRemoteLoadCurrentUserInfo();
   const setSpotifyAuthorizationCode = setSpotifyAuthorizationCodeAdapter;
   const remoteGetMyPlaylists = makeRemoteGetMyPlaylists();
+  const remoteGetRecentlyPlayed = makeRemoteGetRecentlyPlayed();
 
   return (
     <Home
@@ -19,6 +21,7 @@ export const HomePageFactory = ({}: HomePageFactoryProps) => {
         remoteLoadCurrentUserInfo,
         setSpotifyAuthorizationCode,
         remoteGetMyPlaylists,
+        remoteGetRecentlyPlayed,
       }}
     />
   );
