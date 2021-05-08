b35a10fe2ca4bf9b10dac5d09003fa6b4f230b47
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-07 22:27:57 -0300

    refactor: minifiyed interface using common types

--- src/domain/spotify/player/currently-playing-track/currently-playing-track.ts
+++ src/domain/spotify/player/currently-playing-track/currently-playing-track.ts
@@ -1,74 +1,30 @@
-export interface CurrentlyPlayingTrackUser {
-  context: {
-    external_urls: {
-      spotify: string;
-    };
-    href: string;
-    type: string;
-    uri: string;
+import { Album, Artist, Context, Image, ExternalUrls } from "@/domain/spotify";
+
+interface Item {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: {
+    isrc: string;
   };
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+export interface CurrentlyPlayingTrackUser {
+  context: Context;
   timestamp: number;
   progress_ms: number;
   is_playing: boolean;
   currently_playing_type: string;
-  item?: {
-    album: {
-      album_type: string;
-      external_urls: {
-        spotify: string;
-      };
-      href: string;
-      id: string;
-      images: [
-        {
-          height: number;
-          url: string;
-          width: number;
-        },
-        {
-          height: number;
-          url: string;
-          width: number;
-        },
-        {
-          height: number;
-          url: string;
-          width: number;
-        }
-      ];
-      name: string;
-      type: string;
-      uri: string;
-    };
-    artists: [
-      {
-        external_urls: {
-          spotify: string;
-        };
-        href: string;
-        id: string;
-        name: string;
-        type: string;
-        uri: string;
-      }
-    ];
-    available_markets: string[];
-    disc_number: number;
-    duration_ms: number;
-    explicit: boolean;
-    external_ids: {
-      isrc: string;
-    };
-    external_urls: {
-      spotify: string;
-    };
-    href: string;
-    id: string;
-    name: string;
-    popularity: number;
-    preview_url: string;
-    track_number: number;
-    type: string;
-    uri: string;
-  };
+  item?: Item;
 }
