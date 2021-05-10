840d73c73ad3b230583982fa936ad43a2e4f4f58
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-09 23:46:11 -0300

    refactor: moved sut to mock file

--- __tests__/unit/data/usecases/browse/featured-playlist.spec.ts
+++ __tests__/unit/data/usecases/browse/featured-playlist.spec.ts
@@ -1,23 +1,11 @@
 import faker from "faker";
-import { RemoteBrowseFeaturedPlaylist } from "@/data/usecases";
-import { BrowseFeaturedPlaylists } from "@/domain/usecases";
-import { HttpClientSpy } from "__tests__/mocks";
-
-const makeSut = (url = faker.internet.url()) => {
-  const httpClientSpy = new HttpClientSpy<BrowseFeaturedPlaylists.Model>();
-
-  const sut = new RemoteBrowseFeaturedPlaylist(url, httpClientSpy);
-  return {
-    sut,
-    httpClientSpy,
-  };
-};
+import { makeBrowseFeaturedPlaylistsSut } from "__tests__/mocks/data/usecase/browse/featured-playlists/mock-featured-playlists";
 
 describe("FeaturedPlaylist", () => {
   describe("load", () => {
     test("Espero que faça a chamada com os parâmetros corretos", async () => {
       const url = faker.internet.url();
-      const { httpClientSpy, sut } = makeSut(url);
+      const { httpClientSpy, sut } = makeBrowseFeaturedPlaylistsSut(url);
 
       httpClientSpy.response = {
         message: "Its featured playlists",
