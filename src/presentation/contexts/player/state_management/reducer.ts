e8379a931e11b9db0e427f94b065553de35c3fa9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-05-10 23:20:27 -0300

    chore: added reducer actions

--- src/presentation/contexts/player/state_management/reducer.ts
+++ src/presentation/contexts/player/state_management/reducer.ts
@@ -12,8 +12,59 @@ export const initialState: PlayerStates = {
   },
 };
 
-export function playerReducer(state: PlayerStates, action: PlayerActions) {
+export function playerReducer(
+  state: PlayerStates,
+  action: PlayerActions
+): PlayerStates {
   switch (action.type) {
+    case "@player/update-metadatas": {
+      const { currentPlaying } = action.payload;
+
+      return {
+        ...state,
+        currentPlaying,
+      };
+    }
+
+    case "@player/update-playback-state": {
+      const { playerState } = action.payload;
+
+      return {
+        ...state,
+        playbackState: playerState,
+      };
+    }
+
+    case "@player/update-progress-state": {
+      const { progressState } = action.payload;
+
+      return {
+        ...state,
+        progressState,
+      };
+    }
+
+    case "@player/update-timers": {
+      const { current, total } = action.payload;
+
+      return {
+        ...state,
+        timers: {
+          current,
+          total,
+        },
+      };
+    }
+
+    case "@player/update-total-duration": {
+      const { totalDuration } = action.payload;
+
+      return {
+        ...state,
+        totalDuration,
+      };
+    }
+
     default:
       return state;
   }
